8070c5e2299434f3e57e12b2495ee4dbc8a71f8b
kimnamlhn <kimnam.cpc@gmail.com>
2021-03-27 20:19:46 +0700

    test server

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require("path");
 
 const app = express();
 
+app.use('/', function(){
+    app.get("/user")
+})
 // app.use(express.json());
 // app.use(express.urlencoded({
 //     extended: true
