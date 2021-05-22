f7a5e9371d92d9244a5dc7e7d077f83c05986faf
kimnamlhn <kimnam.cpc@gmail.com>
2021-05-22 23:42:51 +0700

    partail layouts

--- middlewares/view.mdw.js
+++ middlewares/view.mdw.js
@@ -3,7 +3,9 @@ var exphbs  = require('express-handlebars');
 module.exports = (app) => {
     app.engine('hbs', exphbs({
         defaultLayout: 'main.hbs',
-        extname: '.hbs'
+        extname: '.hbs',
+        layoutsDir: 'views/_layouts',
+        partialsDir: 'views/_partials',
       }
       ));
       app.set('view engine', 'hbs');
