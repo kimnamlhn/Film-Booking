02c94adaaabc9cb66d37bf6f8d80d7250e12a64e
kimnamlhn <kimnam.cpc@gmail.com>
2021-05-27 00:05:23 +0700

    admin pages

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -6,6 +6,18 @@ const router = express.Router();
 router.get('/movie_manage', async function (req, res) {
         res.render('vwAdmin/movie_manage');
 });
+router.get('/cinema_manage', async function (req, res) {
+        res.render('vwAdmin/cinema_manage');
+});
+router.get('/cineplex_manage', async function (req, res) {
+        res.render('vwAdmin/cineplex_manage');
+});
+router.get('/showtime_manage', async function (req, res) {
+        res.render('vwAdmin/showtime_manage');
+});
+router.get('/statistical_manage', async function (req, res) {
+        res.render('vwAdmin/statistical_manage');
+});
 
 
 module.exports = router;
