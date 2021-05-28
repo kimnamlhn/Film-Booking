8b87194b40aedbf6bdfd2bb4f0f9aad5f1a2d4a5
kimnamlhn <kimnam.cpc@gmail.com>
2021-05-28 23:32:00 +0700

    add demo ticket history and ticket booked

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,12 @@ const router = express.Router();
 router.get('/profile', async function(req,res){
     res.render('vwUser/profile')
 });
+router.get('/ticket-booked', async function(req,res){
+    res.render('vwUser/ticketBooked')
+});
+router.get('/ticket-history', async function(req,res){
+    res.render('vwUser/ticketHistory')
+});
 
 
 
